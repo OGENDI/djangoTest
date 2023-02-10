7719bb63738d2bb6eae1dd1358d5a556874a0f2d
OGENDI <demokonge17@gmail.com>
2023-02-11 00:34:17 +0300

    🔢 Getting to use views.py and html template

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'name' : 'Derick',
+        'age' : 28,
+        'nationality' : 'Kenya'
+    }
+    return render(request, 'index.html', context)
