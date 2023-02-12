3c003dced96e0ca6dad63003f9801d3830296137
OGENDI <demokonge17@gmail.com>
2023-02-12 11:48:42 +0300

    🔢 Simple text counter app

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -8,4 +8,10 @@ def index(request):
         'age' : 28,
         'nationality' : 'Kenya'
     }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
+
+
+def counter(request):
+    text = request.GET['text']
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html', {'amount': amount_of_words})
